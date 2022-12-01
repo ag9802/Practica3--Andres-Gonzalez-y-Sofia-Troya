b25a2f4917a206955f75806b8a69ec40e5995374
ag9802 <118699120+ag9802@users.noreply.github.com>
2022-11-30 21:54:10 -0400

    Practica3- Andres Gonzalez y Sofia Troya-Seccion 03

---
+++ Practica3.2.py
@@ -0,0 +1,10 @@
+n = int(input('ingresar el valor en entero:'))
+acumulator = 0
+k = 1
+
+while k < n :
+    k= k+1
+    acumulator = acumulator + 1
+    
+print('la sumatoria de numeros desde 1 hasta' + str(n) + 'es:' + str(k+10) )
+
